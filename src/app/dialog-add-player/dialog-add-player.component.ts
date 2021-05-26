5cce4d47ab7883b682fb2238a7e8e506051362fe
kailong <wengkailong@gmail.com>
2021-05-26 14:34:05 +0200

    first playable version

---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-dialog-add-player',
+  templateUrl: './dialog-add-player.component.html',
+  styleUrls: ['./dialog-add-player.component.scss']
+})
+export class DialogAddPlayerComponent implements OnInit {
+  name: string = '';
+  constructor(public dialogRef: MatDialogRef<DialogAddPlayerComponent>) { }
+
+  ngOnInit(): void {
+  }
+
+  onNoClick(){
+    console.log('test');
+    this.dialogRef.close();
+  }
+
+}
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss'],
+})
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game;
+
+  constructor(public dialog: MatDialog) {
+    this.game = new Game();
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack[this.game.stack.length - 1];
+      console.log(this.currentCard);
+      this.game.stack.pop();
+      
+      console.log(this.game.playedCards);
+      this.pickCardAnimation = true;
+
+      this.game.currentPlayer++;
+      this.game.currentPlayer = this.game.currentPlayer%this.game.players.length;
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.currentCard);
+      }, 1200);
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent, {
+      width: '250px',
+    });
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if(name && name.length >0){
+        this.game.players.push(name);
+      }
+      
+    });
+  }
+
+
+}
